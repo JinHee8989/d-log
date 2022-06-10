bee581b8fffc5c1db63597a9840f68c0607c40d3
JinHee8989 <wlsgml8989@gmail.com>
2022-06-10 17:31:50 +0900

    add LogContext.js

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, KeyboardAvoidingView, Platform} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import WriteEditor from '../components/WriteEditor';
+import WriteHeader from '../components/WriteHeader';
 
 function WriteScreen() {
-  return <View style={styles.block} />;
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  return (
+    <SafeAreaView style={styles.block}>
+      <KeyboardAvoidingView
+        style={styles.avoidingView}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <WriteHeader />
+        <WriteEditor
+          title={title}
+          body={body}
+          onChangeTitle={setTitle}
+          onChangeBody={setBody}
+        />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {flex: 1, backgroundColor: 'white'},
+  avoidingView: {flex: 1},
 });
 
 export default WriteScreen;
